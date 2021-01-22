69fe9dd6a118824535d2976c31b8f7bac8c42eaf
Lana Milbern <lmilbern91@gmail.com>
2021-01-22 13:47:36 -0500

    Update Guessing Game

--- src/main/java/GuessingGameApp.java
+++ src/main/java/GuessingGameApp.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 public class GuessingGameApp {
 
     public static void main(String[] args) {
+        int number;
+        int count;
+
+        number = 7;
+        count = 1;
+
+        System.out.println("Guess a number.");
+
+        Scanner inputScanner = new Scanner(System.in);
+        int guessedNumber = inputScanner.nextInt();
+        do {
+            if (guessedNumber==number) {
+                System.out.println("You win!");
+                count = 2;
+            } else if(guessedNumber==0) {
+                System.out.println("Guess a number 1-9");
+                guessedNumber = inputScanner.nextInt();
+            }else {
+                count++;
+                System.out.println("Sorry guess again");
+                guessedNumber = inputScanner.nextInt();
+                if (guessedNumber==number){
+                    System.out.println("You win!");
+                    count = 2;}
+                else{
+                    System.out.println("Sorry out of guesses.");
+                }
+        }} while(count<2);
+
+
+
+
+
+
 
     }
 
